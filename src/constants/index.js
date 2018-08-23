a158be74e61855103b7403770203796a21f32fe5
Josh Gretz <jgretz@truefit.io>
2018-08-23 15:31:33 -0400

    add logic to verify private is there

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,11 @@
-export * from './private';
+// apply settings
+const privateKeys = {};
+try {
+  const p = require('./private.js');
+  privateKeys.busApiKey = p.BUS_API_KEY;
+} catch (err) {
+  privateKeys.busApiKey = process.env.busApiKey;
+}
 
+export const BUS_API_KEY = privateKeys.busApiKey;
 export const BUS_FEED = 'Port Authority Bus';
